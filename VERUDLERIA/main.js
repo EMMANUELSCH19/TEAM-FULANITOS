ed588257a374b4ede083189f5587f9509bf6acc8
EMMANUELSCH19 <emmanuel_1320114041@uptecamac.edu.mx>
2021-09-23 16:04:49 -0500

    Disfruten

---
+++ VERUDLERIA/main.js
@@ -0,0 +1,60 @@
+function  agregarProducto1(){
+    event.preventDefault();
+    var producto = document.getElementById("producto1").innerHTML;
+    var precio = document.getElementById("precio1").innerHTML;
+    let ReferenciaTablaAgregar = document.getElementById("tablaProductos");
+    let nuevaFila = ReferenciaTablaAgregar.insertRow(-1);
+    let nuevaRefCelda = nuevaFila.insertCell(0);
+    nuevaRefCelda.textContent = producto;
+    let nuevaRefCelda1 = nuevaFila.insertCell(1);
+    nuevaRefCelda1.textContent = precio;
+    console.log(producto);
+}
+function  agregarProducto2(){
+    event.preventDefault();
+    var producto = document.getElementById("producto2").innerHTML;
+    var precio = document.getElementById("precio2").innerHTML;
+    let ReferenciaTablaAgregar = document.getElementById("tablaProductos");
+    let nuevaFila = ReferenciaTablaAgregar.insertRow(-1);
+    let nuevaRefCelda = nuevaFila.insertCell(0);
+    nuevaRefCelda.textContent = producto;
+    let nuevaRefCelda1 = nuevaFila.insertCell(1);
+    nuevaRefCelda1.textContent = precio;
+    console.log(producto);
+}
+function  agregarProducto3(){
+    event.preventDefault();
+    var producto = document.getElementById("producto3").innerHTML;
+    var precio = document.getElementById("precio3").innerHTML;
+    let ReferenciaTablaAgregar = document.getElementById("tablaProductos");
+    let nuevaFila = ReferenciaTablaAgregar.insertRow(-1);
+    let nuevaRefCelda = nuevaFila.insertCell(0);
+    nuevaRefCelda.textContent = producto;
+    let nuevaRefCelda1 = nuevaFila.insertCell(1);
+    nuevaRefCelda1.textContent = precio;
+    console.log(producto);
+}
+function  agregarProducto4(){
+    event.preventDefault();
+    var producto = document.getElementById("producto4").innerHTML;
+    var precio = document.getElementById("precio4").innerHTML;
+    let ReferenciaTablaAgregar = document.getElementById("tablaProductos");
+    let nuevaFila = ReferenciaTablaAgregar.insertRow(-1);
+    let nuevaRefCelda = nuevaFila.insertCell(0);
+    nuevaRefCelda.textContent = producto;
+    let nuevaRefCelda1 = nuevaFila.insertCell(1);
+    nuevaRefCelda1.textContent = precio;
+    console.log(producto);
+}
+function  agregarProducto5(){
+    event.preventDefault();
+    var producto = document.getElementById("producto5").innerHTML;
+    var precio = document.getElementById("precio5").innerHTML;
+    let ReferenciaTablaAgregar = document.getElementById("tablaProductos");
+    let nuevaFila = ReferenciaTablaAgregar.insertRow(-1);
+    let nuevaRefCelda = nuevaFila.insertCell(0);
+    nuevaRefCelda.textContent = producto;
+    let nuevaRefCelda1 = nuevaFila.insertCell(1);
+    nuevaRefCelda1.textContent = precio;
+    console.log(producto);
+}
